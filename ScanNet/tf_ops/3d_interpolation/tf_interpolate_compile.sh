d80b2ab70a101654e8ed98bb088dde57f89c79d0
JMU-HSF <1261717268@qq.com>
2020-10-28 10:28:59 +0800

    change

--- ScanNet/tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++
@@ -1,5 +0,0 @@
-# TF1.2
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /hom -I /home/jzn/local_install/cuda-8/include -lcudart -L /home/jzn/local_install/cuda-8/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
-# TF1.4
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow/include -I /home/jzn/local_install/cuda-8/include -I /home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow/include/external/nsync/public -lcudart -L /home/jzn/local_install/cuda-8/lib64/ -L/home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- ScanNet/tf_ops/Octant_op/tf_Octant_compile.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-/home/jzn/local_install/cuda-8/bin/nvcc Octant.cu -o Octant_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
-# TF1.4
-g++ -std=c++11 main.cpp Octant_g.cu.o -o tf_Octant_so.so -shared -fPIC -I /home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow/include -I /home/jzn/local_install/cuda-8/include -I /home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow/include/external/nsync/public -lcudart -L /home/jzn/local_install/cuda-8/lib64/ -L/home/jzn/anaconda3/lib/python3.6/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
